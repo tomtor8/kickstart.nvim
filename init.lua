f553c61046a199d93e68c2bb042d2ea60bf8c9ac
Tomas Torday <tomas.torday@gmail.com>
2025-06-03 13:17:09 +0200

    add mini.snippets plugin

--- init.lua
+++ init.lua
@@ -883,6 +883,19 @@ require('lazy').setup({
         },
       }
 
+      -- snippet manager
+      -- snippets are located at the file see below
+      local gen_loader = require('mini.snippets').gen_loader
+      require('mini.snippets').setup {
+        snippets = {
+          -- Load custom file with global snippets first (adjust for Windows)
+          gen_loader.from_file '~/.config/nvim/snippets/global.json',
+
+          -- Load snippets based on current language by reading files from
+          -- "snippets/" subdirectories from 'runtimepath' directories.
+          gen_loader.from_lang(),
+        },
+      }
       -- Require mini.tabline
       require('mini.tabline').setup {
         -- Your tabline configuration here
